10ae0891abd296dc3ad2954068b9ef42ec59b4f7
mondalujjwayini2001-cmyk <mondalujjwayini2001@gmail.com>
2025-10-23 10:20:33 +0530

    1st Lession

---
+++ main.js
@@ -0,0 +1,31 @@
+let Variable = "ajdldjakld"
+const lima = "adjadadklajdl"
+
+"hafhafka 24371351841647104 [[][][]2][1316141704!$%#^%^"
+
+const lala = 33
+
+let bini = 542646
+true
+false
+// []
+// {}
+
+let x = 5
+let y = "5"
+console.log(x + y);
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x / y);
+// console.log(x % y)
+
+if (x === y){
+console.log(" true");
+
+}else{
+    console.log("false");
+    
+}
+
+
+
