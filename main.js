8523e4ddcd3088a7a6c2eef13a2329fb49058b22
mondalujjwayini2001-cmyk <mondalujjwayini2001@gmail.com>
2025-11-01 12:44:11 +0530

    learn ternery oprator & if else & logical oprators

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ false
 // []
 // {}
 
+
 let x = 5
 let y = "5"
 console.log(x + y);
@@ -19,13 +20,47 @@ console.log(x + y);
 // console.log(x / y);
 // console.log(x % y)
 
-if (x === y){
-console.log(" true");
+// if (x === y){
+// console.log(" true");
 
-}else{
-    console.log("false");
-    
+// }else{
+//     console.log("false");
+
+// }
+
+let num = 1
+
+if (num > 9) {
+    console.log("yes it is less than 9");
+
+} else {
+    console.log("no it is not grather than 9");
 }
 
+let username = "admin"
+let password = "12345"
 
+// if (!username) {
+//     dagidaihf
+// } else {
+//     iogo
+// }
+
+if (username && password) {
+    console.log("login successful");
+} else {
+    log("login failed");
+}
+
+let age = 18
+let hasID = true
+
+if (age || hasID) {
+    console.log("at least one value is present");
+} else {
+console.log("no values are present");
+}
+
+// ternary operator
+age>18? console.log("adult") : console.log("minor");
 
